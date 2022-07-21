86737cba25f5b370d509f9f6dfa2aa4bc4d16f7e
harsh <harsh@DESKTOP-V6TJNO1>
2022-07-21 15:21:34 -0400

    Add project files.

---
+++ Naresh-web/Pages/Intro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Naresh_web.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
